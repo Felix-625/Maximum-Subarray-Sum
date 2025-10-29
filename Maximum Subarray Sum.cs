cbfb5388ab8f463b47b9c5e2d82f8fd6cd4ea0e2
Felix-625 <almerofelix62@gmail.com>
2025-10-29 05:15:00 -0700

    Create Maximum Subarray Sum.cs

---
+++ Maximum Subarray Sum.cs
@@ -0,0 +1,80 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'maximumSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. LONG_INTEGER_ARRAY a
+     *  2. LONG_INTEGER m
+     */
+
+    public static long maximumSum(List<long> a, long m)
+    {
+        long maxSum = 0;
+    long currentSum = 0;
+    SortedSet<long> prefixSums = new SortedSet<long>();
+    
+    for (int i = 0; i < a.Count; i++)
+    {
+        currentSum = (currentSum + a[i]) % m;
+        maxSum = Math.Max(maxSum, currentSum);
+        
+        // Look for the smallest prefix sum that is larger than current sum
+        var nextLarger = prefixSums.GetViewBetween(currentSum + 1, long.MaxValue).FirstOrDefault();
+        if (nextLarger != default(long))
+        {
+            maxSum = Math.Max(maxSum, (currentSum - nextLarger + m) % m);
+        }
+        
+        prefixSums.Add(currentSum);
+    }
+    
+    return maxSum;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            long m = Convert.ToInt64(firstMultipleInput[1]);
+
+            List<long> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt64(aTemp)).ToList();
+
+            long result = Result.maximumSum(a, m);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
